d0f26a79606ee49f34351c2da2da9adb201caa00
Vincent Tourraine <me@vtourraine.net>
2014-05-03 19:00:27 +0200

    Add test project

---
+++ Tests/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// NSString+VTSubstringOccurrences
+#define COCOAPODS_POD_AVAILABLE_NSString_VTSubstringOccurrences
+#define COCOAPODS_VERSION_MAJOR_NSString_VTSubstringOccurrences 0
+#define COCOAPODS_VERSION_MINOR_NSString_VTSubstringOccurrences 0
+#define COCOAPODS_VERSION_PATCH_NSString_VTSubstringOccurrences 1
+
